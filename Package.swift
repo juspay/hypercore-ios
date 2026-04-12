2e03584287349cdfa3f7031696d805978f604e91
Titan Jenkins <titan.a@juspay.in>
2026-04-12 14:13:59 +0530

    Updated Package for version 1.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperCore",
-            url: "https://public.releases.juspay.in/release/ios/hyper-core/1.0.0/HyperCore.zip",
-            checksum: "3627f1ed80087ad374d4f256b6b47fc8b949dfec00540af575410e9755d6c410"
+            url: "https://public.releases.juspay.in/release/ios/hyper-core/1.0.1/HyperCore.zip",
+            checksum: "bf0dfba5271ecca4b820b65101adf8f2ec01abb33758a79f8e51347cc226f77a"
         )
     ]
 )
